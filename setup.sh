d4648172095b15791782d31a46231bf5bbd0bb1a
moui66744 <whaoyu09@163.com>
2023-06-20 22:26:08 +0800

    use temp file name instead of hard-coded name

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 zsh_url='https://raw.githubusercontent.com/romkatv/zsh-bin/master/install'
-zsh_installer='zsh_installer'
+zsh_installer=$(mktemp ./install-XXXX.sh)
 
 ohmyzsh_url='https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh'
-ohmyzsh_installer='ohmyzsh_installer'
+ohmyzsh_installer=$(mktemp ./install-XXXX.sh)
 
 run() {
     if eval $1; then
@@ -18,9 +18,21 @@ run() {
 # zsh
 run "curl -fsSL $zsh_url > $zsh_installer" "fetch zsh installer"
 run "sh $zsh_installer -d ~/.local" "install zsh"
+rm -f $zsh_installer
 
 export PATH=$HOME/.local/bin:$PATH
+
+# TODO plugins
+# autosuggestions
+# syntax-highlight
+
+# TODO p10k
+
+# TODO copy PATH
+
+# TODO copy bash history
 
 # oh-my-zsh
 run "curl -fsSL $ohmyzsh_url > $ohmyzsh_installer" "fetch ohmyzsh installer" "fetch ohmyzsh installer"
 run "sh $ohmyzsh_installer" "install ohmyzsh"
+rm -f $ohmyzsh_installer
